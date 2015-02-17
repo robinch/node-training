1621550440ee27f29771c97ee58bc70ab49de188
Robin Chowdhury <robinch88@gmail.com>
2015-02-17 17:24:50 +0100

    done with http-client

---
+++ learnyounode/http_client.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+http.get(process.argv[2], function(response){
+	response.setEncoding("utf8");
+	response.on("data", console.log);
+	response.on("error", console.error);
+	response.on("end", function(end){
+
+	});
+
+});
